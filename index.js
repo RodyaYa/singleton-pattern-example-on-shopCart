cc57dc2b9a1188614f2c5921288d232303dd1a98
Rodion <rusyaloveee@gmail.com>
2021-05-28 20:45:01 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 import { createCart, Cart } from "./cart/Cart.js";
-
 const newCart = new Cart();
-
+let elementsWithEventListener = [];
 const addToCartButtons = document.querySelectorAll('[data-btn="add"]');
 const showCartButtons = document.querySelectorAll('[data-btn="show"]');
 const ranges = document.querySelectorAll('[data-range="input"]');
 const selects = document.querySelectorAll('[data-select="order"]');
 
-function eventAdder(elems, type, func) {
-  elems.forEach((elem) => {
-    elem.addEventListener(type, func);
-  });
-}
-
 eventAdder(addToCartButtons, "click", addToCartHandler);
 eventAdder(showCartButtons, "click", showCart);
 eventAdder(selects, "input", selectHandler);
-eventAdder(ranges, "input", rangeHandler);
+eventAdder(ranges, "input", rangeRender);
 
-let order = {
+window.onbeforeunload = eventRemover;
+
+function eventAdder(elems, type, func) {
+  elems.forEach((elem) => {
+    elem.addEventListener(type, func);
+    elementsWithEventListener.push({ elem, type, func });
+  });
+}
+
+function eventRemover(event) {
+  elementsWithEventListener.forEach(({ elem, type, func }) => {
+    elem.removeEventListener(type, func);
+  });
+}
+
+let $order = {
   title: "",
   id: 0,
   amount: 0,
@@ -29,21 +37,27 @@ function showCart() {
 }
 
 function selectHandler(e) {
-  console.log("selected");
-  const title = e.target.selectedOptions[0].textContent;
-  const id = e.target.selectedOptions[0].value;
-  order.id = id;
-  order.title = title;
+  const { value: id, textContent: title } = e.target.selectedOptions[0];
+
+  $order.id = id;
+  $order.title = title;
 }
 
-function rangeHandler(e) {
-  const value = e.target.value;
-  order.amount = value;
+function rangeRender(e) {
+  const { value } = e.target;
+  $order.amount = value;
   e.target.previousElementSibling.textContent = `Количество: ${value}`;
 }
 
 function addToCartHandler(e) {
   e.preventDefault();
-  console.log("added");
-  newCart.addToCart(order);
+  newCart.addToCart($order);
+  $order = {};
+  resetFields(e);
+
+  function resetFields(e) {
+    e.target.previousElementSibling.value = 0;
+    e.target.previousElementSibling.previousElementSibling.textContent =
+      "Количество 0";
+  }
 }
